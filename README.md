968f213b69951ccd0588a40fe26d2497476a1e46
Fr33zyb01 <umarbanbhan123@outlook.com>
2020-08-05 00:14:54 +0200

    added track, smoother steering, added read me

--- README.md
+++ README.md
@@ -1,1 +1,140 @@
-# Car AI
+<!--
+*** Thanks for checking out this README Template. If you have a suggestion that would
+*** make this better, please fork the car-ai and create a pull request or simply open
+*** an issue with the tag "enhancement".
+*** Thanks again! Now go create something AMAZING! :D
+***
+***
+***
+*** To avoid retyping too much info. Do a search and replace for the following:
+*** umar-b, car-ai, @DesignsFreezy, umarbanbhan123@outlook.com
+-->
+
+
+
+
+
+<!-- PROJECT SHIELDS -->
+<!--
+*** I'm using markdown "reference style" links for readability.
+*** Reference links are enclosed in brackets [ ] instead of parentheses ( ).
+*** See the bottom of this document for the declaration of the reference variables
+*** for contributors-url, forks-url, etc. This is an optional, concise syntax you may use.
+*** https://www.markdownguide.org/basic-syntax/#reference-style-links
+-->
+<!-- [![Contributors][contributors-shield]][contributors-url]
+[![Forks][forks-shield]][forks-url]
+[![Stargazers][stars-shield]][stars-url]
+[![Issues][issues-shield]][issues-url]
+[![MIT License][license-shield]][license-url]
+[![LinkedIn][linkedin-shield]][linkedin-url] -->
+
+
+
+<!-- PROJECT LOGO -->
+<br />
+<p align="center">
+  <a href="https://github.com/umar-b/car-ai">
+    <img src="images/logo.png" alt="Logo" width="80" height="80">
+  </a>
+
+  <h3 align="center">CAR AI</h3>
+
+  <p align="center">
+    Self driving car simulation in python.
+    <br />
+    <a href="https://github.com/umar-b/car-ai"><strong>Explore the docs »</strong></a>
+    <br />
+    <br />
+    <a href="https://github.com/umar-b/car-ai">View Demo</a>
+    ·
+    <a href="https://github.com/umar-b/car-ai/issues">car-airt Bug</a>
+    ·
+    <a href="https://github.com/umar-b/car-ai/issues">Request Feature</a>
+  </p>
+</p>
+
+
+
+<!-- TABLE OF CONTENTS -->
+## Table of Contents
+
+- [Table of Contents](#table-of-contents)
+- [About The Project](#about-the-project)
+- [Getting Started](#getting-started)
+  - [Prerequisites](#prerequisites)
+  - [Installation](#installation)
+  - [TO DO](#to-do)
+- [License](#license)
+
+
+
+<!-- ABOUT THE PROJECT -->
+## About The Project
+
+<!-- [![Product Name Screen Shot][product-screenshot]](https://example.com) -->
+
+This Project is about simulating a self driving car with randomized tracks. The whole project was written in python.
+
+
+
+<!-- GETTING STARTED -->
+## Getting Started
+
+To get a local copy up and running follow these simple steps.
+
+### Prerequisites
+
+The following libraries are needed for the project to work.
+
+* python 3.7.x
+* numpy
+* pygame
+* scipy
+* tensorflow
+
+This is an example of how to list things you need to use the software and how to install them.
+
+### Installation
+
+1. Clone the car-ai project
+```sh
+git clone https://github.com/umar-b/car-ai.git
+```
+2. Install pip packages
+```sh
+pip install -r requirements.txt
+```
+
+### TO DO
+
+* add sensors/lidar
+* implement ai
+* finish track generation
+
+
+
+
+<!-- LICENSE -->
+## License
+
+Distributed under the MIT License. See `LICENSE` for more information.
+
+
+
+
+<!-- MARKDOWN LINKS & IMAGES -->
+<!-- https://www.markdownguide.org/basic-syntax/#reference-style-links -->
+[contributors-shield]: https://img.shields.io/github/contributors/othneildrew/Best-README-Template.svg?style=flat-square
+[contributors-url]: https://github.com/othneildrew/Best-README-Template/graphs/contributors
+[forks-shield]: https://img.shields.io/github/forks/othneildrew/Best-README-Template.svg?style=flat-square
+[forks-url]: https://github.com/othneildrew/Best-README-Template/network/members
+[stars-shield]: https://img.shields.io/github/stars/othneildrew/Best-README-Template.svg?style=flat-square
+[stars-url]: https://github.com/othneildrew/Best-README-Template/stargazers
+[issues-shield]: https://img.shields.io/github/issues/othneildrew/Best-README-Template.svg?style=flat-square
+[issues-url]: https://github.com/othneildrew/Best-README-Template/issues
+[license-shield]: https://img.shields.io/github/license/othneildrew/Best-README-Template.svg?style=flat-square
+[license-url]: https://github.com/othneildrew/Best-README-Template/blob/master/LICENSE.txt
+[linkedin-shield]: https://img.shields.io/badge/-LinkedIn-black.svg?style=flat-square&logo=linkedin&colorB=555
+[linkedin-url]: https://linkedin.com/in/othneildrew
+[product-screenshot]: images/screenshot.png
--- game.py
+++ game.py
@@ -5,45 +5,62 @@ import os
 
 
 class Car:
-    def __init__(self, x, y):
+    def __init__(self, x, y, car_image, track):
+        self.car_image = car_image
+        self.track = track
+        self.rotated = car_image
         self.pos = Vector2(x, y)
         self.speed = 0.0
         self.angle = 0.0
         self.distance = 0
         self.time_spent = 0
 
-    def update(self, dt):
-        self.pos += ((math.cos(math.radians(360 - self.angle)) * self.speed) * dt,
-                     (math.sin(math.radians(360 - self.angle)) * self.speed) * dt)
+    # rotate the image from the center
 
+    def draw(self, screen):
+        blitRotate(screen, self.car_image, self.pos,
+                   (self.car_image.get_rect().width/2, self.car_image.get_rect().height/2), self.angle)
+
+    def update(self, dt):
+
+        # deceleration and speed limits
+        self.speed -= 0.5 * dt
+        if self.speed > 15:
+            self.speed = 15
+        if self.speed < 1:
+            self.speed = 0
+
+        # update position
+        self.pos.x += (math.cos(math.radians(-self.angle)) * self.speed) * dt
+        self.pos.y += (math.sin(math.radians(-self.angle)) * self.speed) * dt
+
+        # log distance and time
         self.distance += self.speed
         self.time_spent += 1
-
-        if self.pos[0] < 20:
-            self.pos[0] = 20
-        elif self.pos[0] > 1280 - 120:
-            self.pos[0] = 1280 - 120
-
-        if self.pos[1] < 20:
-            self.pos[1] = 20
-        elif self.pos[1] > 720 - 120:
-            self.pos[1] = 720 - 120
 
 
 class Game:
     def __init__(self):
         pygame.init()
+
+        # load paths
+        resources_dir = os.path.realpath("./resources")
+        map_path = os.path.join(resources_dir, "Track.jpg")
+        image_path = os.path.join(resources_dir, "car_small.png")
+
+        # load images
+        map_image = pygame.image.load(map_path)
+        car_image = pygame.image.load(image_path)
+
         pygame.display.set_caption("Car AI")
+
         self.screen = pygame.display.set_mode((1280, 720))
         self.clock = pygame.time.Clock()
-        self.car = Car(0, 0)
-        self.ticks = 128
+        self.car = Car(0, 0, car_image, map_image)
+        self.ticks = 128  # 128 ticks smoother than 64??
         self.exit = False
 
     def run(self):
-        resources_dir = os.path.realpath("./resources")
-        image_path = os.path.join(resources_dir, "car_small.png")
-        car_image = pygame.image.load(image_path)
         while not self.exit:
             dt = self.clock.get_time() / 100
 
@@ -56,34 +73,54 @@ class Game:
             pressed = pygame.key.get_pressed()
 
             if pressed[pygame.K_UP]:
-                if self.car.speed > 10:
-                    self.car.speed = 10
-                elif self.car.speed < 1:
-                    self.car.speed = 1
-                else:
-                    self.car.speed += 2 * dt
-
-            if pressed[pygame.K_DOWN]:
-                if self.car.speed < -10:
-                    self.car.speed = -10
-                else:
-                    self.car.speed -= 1 * dt
+                self.car.speed += 10 * dt
 
             if pressed[pygame.K_RIGHT]:
-                self.car.angle -= 0.5
+                self.car.angle -= 15 * dt
             elif pressed[pygame.K_LEFT]:
-                self.car.angle += 0.5
+                self.car.angle += 15 * dt
 
-            # Logic
             self.car.update(dt)
 
             # Drawing
-            self.screen.fill((0, 0, 0))
-            rotated = pygame.transform.rotate(car_image, self.car.angle)
-            self.screen.blit(rotated, self.car.pos)
+            self.screen.blit(self.car.track, (0, 0))
+            self.car.draw(self.screen)
             pygame.display.flip()
             self.clock.tick(self.ticks)
-            print("Time spent: " + str(self.car.time_spent), end="\r")
-            print("Distance: " + str(self.car.distance), end="\r")
-            print("Speed: " + str(self.car.speed), end="\r", flush=True)
+            # print("Time spent: " + str(self.car.time_spent), end="\r", flush=True)
+            # print("Distance: " + str(self.car.distance), end="\r", flush=True)
+            # print("Speed: " + str(self.car.speed), end="\r", flush=True)
         pygame.quit()
+
+# function for rotating image from stackoverflow
+
+
+def blitRotate(surf, image, pos, originPos, angle):
+
+    # calcaulate the axis aligned bounding box of the rotated image
+    w, h = image.get_size()
+    box = [pygame.math.Vector2(p) for p in [(0, 0), (w, 0), (w, -h), (0, -h)]]
+    box_rotate = [p.rotate(angle) for p in box]
+    min_box = (min(box_rotate, key=lambda p: p[0])[
+               0], min(box_rotate, key=lambda p: p[1])[1])
+    max_box = (max(box_rotate, key=lambda p: p[0])[
+               0], max(box_rotate, key=lambda p: p[1])[1])
+
+    # calculate the translation of the pivot
+    pivot = pygame.math.Vector2(originPos[0], -originPos[1])
+    pivot_rotate = pivot.rotate(angle)
+    pivot_move = pivot_rotate - pivot
+
+    # calculate the upper left origin of the rotated image
+    origin = (pos[0] - originPos[0] + min_box[0] - pivot_move[0],
+              pos[1] - originPos[1] - max_box[1] + pivot_move[1])
+
+    # get a rotated image
+    rotated_image = pygame.transform.rotate(image, angle)
+
+    # rotate and blit the image
+    surf.blit(rotated_image, origin)
+
+    # draw rectangle around the image
+    # pygame.draw.rect(surf, (255, 0, 0),
+    #                  (*origin, *rotated_image.get_size()), 2)
